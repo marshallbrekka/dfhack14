b63551adf7c99582c8fc70ba12312d1c7148bb31
Marshall Brekka <marshallsmedia@gmail.com>
2014-10-11 10:11:37 -0700

    add some debugging

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [compojure "1.1.8"]
                  [ring/ring-jetty-adapter "1.2.2"]
                  [environ "0.5.0"]
-                 [org.clojure/tools.logging "0.2.4"]]
+                 [org.clojure/tools.logging "0.2.4"]
+                 [clj-oauth "1.5.1"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
