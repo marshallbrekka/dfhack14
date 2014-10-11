1c1245cf1d473cdd3f2d10d39e6f3a0cec2f88ef
Kleinlieu <klein@kleinlieu.com>
2014-10-11 11:49:11 -0700

    Adding Leaderboard and Question Views

---
+++ iOS/Kinder/Kinder/LeaderboardViewController.m
@@ -0,0 +1,37 @@
+//
+//  LeaderboardViewController.m
+//  Kinder
+//
+//  Created by Klein Lieu on 10/11/14.
+//  Copyright (c) 2014 Klein Lieu. All rights reserved.
+//
+
+#import "LeaderboardViewController.h"
+
+@interface LeaderboardViewController ()
+
+@end
+
+@implementation LeaderboardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ iOS/Kinder/Kinder/QuestionViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuestionViewController.h
+//  Kinder
+//
+//  Created by Klein Lieu on 10/11/14.
+//  Copyright (c) 2014 Klein Lieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionViewController : UIViewController
+
+@end
